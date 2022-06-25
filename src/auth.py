b8358e6d33ee092b1984f5488252b7b8fbd11512
Cyber2f08 <cyber.2f08@gmail.com>
2022-06-25 13:07:59 +0700

    guest removed.

--- src/auth.py
+++ src/auth.py
@@ -5,10 +5,5 @@ Worker = [
         "Username": "Cyb2f",
         "Password": "ZIPOF01",
         "ID": "1"
-    },
-    {
-        "Username": "Guest",
-        "Password": "12345",
-        "ID": "2"
     }
 ]
