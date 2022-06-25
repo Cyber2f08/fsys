80d0e7a343fe3a4f284e743612fff9cb3a7ed77c
Cyber2f08 <cyber.2f08@gmail.com>
2022-06-25 13:07:14 +0700

    cleaning code.

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ from src import comms
 # For steps verification. informations...
 log_ld = []
 log_lg = []
+m = print
 
-print(
-'''
-  _._     _,-'""`-._         | Meow, Oh Hi!                          |
- (,-.`._,'(       |\`-/|     | Welcome to FSYS,                      |
-     `-.-' \ )-`( , o o)     | Please wait while the systems loads.. |
-           `-    \`_`"'-     | - Cyb2f                               |
-'''
-)
-print(" => Loading... ")
-sld = comms.load()
-log_ld.append(sld)
-xrint(Information, "Login is screen is going to pop up below this.")
-toksr = comms.login()
-log_lg.append(toksr)
-comms.main(toks=toksr, ld=log_ld, lg=log_lg)
+def _wcbanner():
+    print(
+  '''
+    _._     _,-'""`-._         | Meow, Oh Hi!                          |
+  (,-.`._,'(       |\`-/|     | Welcome to FSYS,                      |
+      `-.-' \ )-`( , o o)     | Please wait while the systems loads.. |
+            `-    \`_`"'-     | - Cyb2f                               |
+  '''
+    )
+
+if __name__ == "__main__":
+  
+  _wcbanner()
+  m(" => Loading... ")
+
+  sld = comms.load()  
+  xrint(Information, "Login is screen is going to pop up below this.")
+  
+  toksr = comms.login()
+  
+  log_lg.append(toksr)
+  log_ld.append(sld)
+
+  comms.main(toks=toksr, ld=log_ld, lg=log_lg)
